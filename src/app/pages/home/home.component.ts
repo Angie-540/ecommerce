e3ea4404460cd9dee20f7bbbf1e93f93335e0d17
Angie-540 <janiceangella21@gmail.com>
2023-07-02 16:29:43 +0300

    home component

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,28 @@
+import { ProductService } from './../../services/product.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit{
+
+  productList: any [] = [];
+  constructor(private productService: ProductService) {
+  }
+
+  ngOnInit(): void {
+    debugger;
+    this.loadAllProducts();
+    
+  }
+
+  loadAllProducts() {
+    this.productService.getAllProducts().subscribe((result:any)=> {
+      this.productList = result.data;
+
+    })
+  }
+
+}
---
+++ src/app/services/product.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http:HttpClient) { }
+
+  getAllProducts(): Observable<any[]> {
+    return this.http.get<any[]>("http://onlinetestapi.gerasim.in/api/Ecomm/GetAllProducts");
+  }
+}
